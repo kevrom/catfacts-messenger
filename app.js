2d8a4bcb314f6333901ab0d680e424ae8c95f697
Kevin Romano <kevin.romano@seagate.com>
2016-05-10 11:03:31 -0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,81 @@
+import express from 'express'
+import path from 'path'
+import logger from 'morgan'
+import bodyParser from 'body-parser'
+import settings from './settings'
+import fetch from 'node-fetch'
+import { Subject, Observable } from 'rxjs'
+import facts from './catfacts.json'
+
+const app = express()
+
+const messageUrl = `https://graph.facebook.com/v2.6/me/messages?access_token=${settings.facebook.pageToken}`
+
+const generateFact = () => facts[Math.floor(Math.random() * facts.length)]
+
+const messages$ = new Subject()
+  .mergeAll()
+  .filter(m => !m.delivery)
+  .map(m => ({
+    sender: m.sender.id,
+    text: m.message.text
+  }))
+  .mergeMap(m => Observable.fromPromise(fetch(messageUrl, {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      recipient: { id: m.sender },
+      message: { text: generateFact() }
+    })
+  }).then(res => res.json())))
+
+messages$.subscribe(message => {
+  console.log(message)
+}, err => {
+  console.log(err)
+})
+
+app.use(logger('dev'))
+app.use(bodyParser.json())
+
+app.get('/webhook', (req, res) => {
+  if (req.query['hub.verify_token'] === settings.facebook.verifyToken) {
+    res.send(req.query['hub.challenge'])
+  } else {
+    res.send('Error, wrong validation token')
+  }
+})
+
+app.post('/webhook', (req, res) => {
+  messages$.next(req.body.entry[0].messaging)
+  res.sendStatus(200)
+})
+
+app.use((req, res, next) => {
+  const err = new Error('Not Found')
+  err.status = 404
+  next(err)
+})
+
+if (app.get('env') === 'development') {
+  app.use((err, req, res) => {
+    res.status(err.status || 500)
+    res.render('error', {
+      message: err.message,
+      error: err
+    })
+  })
+}
+
+app.use((err, req, res) => {
+  res.status(err.status || 500)
+  res.render('error', {
+    message: err.message,
+    error: {}
+  })
+})
+
+export default app
---
+++ settings.sample.js
@@ -0,0 +1,12 @@
+// change and save this file as settings.js
+
+export default {
+  server: {
+    host: '0.0.0.0',
+    port: 8222
+  },
+  facebook: {
+    verifyToken: 'i_believe_i_can_fly',
+    pageToken: '<your page token goes here>'
+  }
+}
