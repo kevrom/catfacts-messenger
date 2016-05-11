ed3c7327a9d164a6b7213ae47b5d53eb997a34d9
Kevin Romano <kevin.romano@seagate.com>
2016-05-11 04:19:01 -0700

    removing unnecessary routes, add healthcheck

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 import express from 'express'
-import path from 'path'
 import logger from 'morgan'
 import bodyParser from 'body-parser'
 import settings from './settings'
@@ -54,28 +53,6 @@ app.post('/webhook', (req, res) => {
   res.sendStatus(200)
 })
 
-app.use((req, res, next) => {
-  const err = new Error('Not Found')
-  err.status = 404
-  next(err)
-})
-
-if (app.get('env') === 'development') {
-  app.use((err, req, res) => {
-    res.status(err.status || 500)
-    res.render('error', {
-      message: err.message,
-      error: err
-    })
-  })
-}
-
-app.use((err, req, res) => {
-  res.status(err.status || 500)
-  res.render('error', {
-    message: err.message,
-    error: {}
-  })
-})
+app.get('/health', (req, res) => res.send({ status: 'ok' }))
 
 export default app
